46422cb703880c9e4884646ab6c67b61aa72155c
kramer <kramer@9a232c37-f711-0410-8274-ad4d7d906d31>
2004-06-16 16:03:51 +0000

    Added the GNU license header.

--- JLibGet/ISISio.java
+++ JLibGet/ISISio.java
@@ -1,3 +1,47 @@
+/*
+ * File:  ISISio.java
+ *
+ * Copyright (C) 2004 Chris M. Bouzek
+ * Adapted to Java from FORTRAN routines 
+ * originally written by Kevin Knowles.  
+ * Modifications to the FORTRAN routines 
+ * were also made by Freddie Akeroyd, ISIS.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Chris Bouzek <coldfusion78@yahoo.com>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA and by
+ * the National Science Foundation under grant number DMR-0218882.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * $Log$
+ * Revision 1.3  2004/06/16 16:03:50  kramer
+ * Added the GNU license header.
+ *
+ * Revision 1.2  2004/04/30 00:18:17  bouzekc
+ * New version.  Old version should not have been in CVS.
+ *
+ */
+
 /*---------------------------------------------------------------------------
  * *** CCLRC ISIS Facility GET Routines ***
  * *** Original routines by Kevin Knowles, modifications by Freddie Akeroyd
@@ -7,11 +51,6 @@
  * DEC/CMS REPLACEMENT HISTORY, Element BYTE_REL_EXPN.FOR
  *
  * Converted to Java by Chris M. Bouzek, February 2004
- *
- * $Log$
- * Revision 1.2  2004/04/30 00:18:17  bouzekc
- * New version.  Old version should not have been in CVS.
- *
  */
 package ISIS.JLibGet;
 
--- JLibGet/IntConvert.java
+++ JLibGet/IntConvert.java
@@ -1,4 +1,38 @@
 /*
+ * File:  IntConvert.java
+ *
+ * Copyright (C) 2004 Chris M. Bouzek
+ * adapted from FORTRAN routines 
+ * originally written by Freddie Akeroyd, ISIS
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Chris Bouzek <coldfusion78@yahoo.com>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA and by
+ * the National Science Foundation under grant number DMR-0218882.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ */
+
+/*
  * File: IntConvert.java
  * Original Author:         Freddie Akeroyd, ISIS
  *
